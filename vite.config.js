30bcd23d6a2af1ecf31032b0be8d73b75f8f9e9a
hellenNico <hellen.nico@gmail.com>
2022-11-14 23:31:43 +0900

    viteの表記変更

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base:'./',
+  assetsDir:'./',
   build: {
-    base:'./',
-    assetsDir:'./',
     outDir: 'docs',
   },
 })
